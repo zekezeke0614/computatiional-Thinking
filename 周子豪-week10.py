ee48ccac5ee1f09df7b7bc16f277db3234bbc3d3
zekezeke0614 <71420659+zekezeke0614@users.noreply.github.com>
2020-11-19 11:12:17 +0800

    Add files via upload

---
+++ 周子豪-week10.py
@@ -0,0 +1,11 @@
+movie_title = "money ball"
+relaease_year = "2011"
+chinese_movie_title = "魔球"
+length = "133分鐘"
+evaluation = "7.6"
+
+print("英文名稱" , movie_title)
+print("年份" , relaease_year)
+print("中文名稱" , chinese_movie_title)
+print("片長" , length)
+print("評價" , evaluation)
